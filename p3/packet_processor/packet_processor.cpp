c07b134e2553c3a8faffcf77ab0dc55d465d3794
manuel mariscal <i72marom@uco.es>
2020-03-17 19:14:53 +0100

    p3 - cola - ok

---
+++ p3/packet_processor/packet_processor.cpp
@@ -0,0 +1,52 @@
+#include "packet_processor.hpp"
+
+PacketProcessor::PacketProcessor(size_t size)
+{
+   //TODO
+}
+
+Response 
+PacketProcessor::process(const Packet &packet)
+{
+    //TODO
+
+//Cuando llega un paquete:
+//
+// 1. quitaremos de la cola todos los que llegaron antes y que
+//    ya han sido procesados.
+// 2. Si no hay espacio en la cola, este paquete será dropped y termina
+//    su procesado retornando Response(true, 0).
+// 3. Sino
+// 3.1 Si la cola está vacía, el paquete será procesado inmediatamente
+//     retornando Response(false, tiempo de llegada)
+// 3.2 Sino será encolado y su tiempo de inicio de proceso será 
+//     por lo tanto el tiempo finalización del procesado del
+//     último paquete encolado retornando 
+//     Response(false, tiempo de finalización del último paquete).
+//
+// Cuestión: En la cola, ¿qué encolamos?; el tiempo de llegada o 
+// el tiempo de finalización.
+
+    return Response(true, 0);
+}
+
+
+/** @brief process the packets and generate a response for each of them.*/
+std::vector <Response>
+process_packets(const std::vector <Packet> &packets,
+                PacketProcessor& p)
+{
+    std::vector <Response> responses;
+    for (size_t i = 0; i < packets.size(); ++i)
+        responses.push_back(p.process(packets[i]));
+    return responses;
+}
+
+/** @brief print the processing start times for the packets.*/
+std::ostream&
+write_responses(std::ostream &out, const std::vector <Response> &responses)
+{
+    for (size_t i = 0; i < responses.size(); ++i)
+        out << (responses[i].dropped ? -1 : responses[i].start_time) << std::endl;
+    return out;
+}
---
+++ p3/packet_processor/queue.hpp
@@ -0,0 +1,164 @@
+#ifndef __Queue_HPP__
+#define __Queue_HPP__
+
+#include <cassert>
+#include <cstdlib>
+#include <stack>
+
+
+/**
+ * @brief ADT Queue.
+ * Models a queue of T.
+ */
+template<class T>
+class Queue
+{
+	public:
+
+		/** @name Life cicle.*/
+		/** @{*/
+
+		/** @brief Create an empty Queue.
+		 * @post is_empty()
+		 */
+		Queue () : head_(NULL), tail_(NULL) {
+			//TODO
+			assert(this->is_empty());
+		}
+
+		/** @brief Destroy a Queue.**/
+		~Queue() {
+			//TODO
+			while (!this->is_empty()) this->deque();
+		}
+
+		/** @}*/
+
+		/** @name Observers*/
+
+		/** @{*/
+
+		/** @brief is the list empty?.*/
+		bool is_empty () const {
+			//TODO
+			return (head_ == NULL);
+			// DONE
+		}
+
+		/** @brief Gets the number of items in the queue.*/
+		size_t size() const {
+			//TODO
+			int cont = 0;
+			nodo *aux;
+			aux = new nodo;
+			aux = head_;
+
+			while ((aux = aux->next) != NULL) cont++;
+
+			return cont;
+
+			// DONE
+		}
+
+		/** @brief get the front item (the oldest one).
+		 * @pre not is_empty()
+		 */
+		const T& front() const {
+			//TODO
+			return head_->value;
+			// DONE
+		}
+
+		/** @brief get the back item (the newest one).
+		 * @pre not is_empty()
+		 */
+		const T& back() const {
+			//TODO
+			assert(!this->is_empty());
+			return tail_->value;
+			// DONE
+		}
+
+		/**@}*/
+
+		/** @name Modifiers*/
+
+		/** @{*/
+
+		/** @brief Insert a new item.
+		 * @post !is_empty()
+		 */
+		void enque(const T& new_it) {
+			//TODO
+
+			// creo un nuevo nodo auxiliar para introducir 
+			// el nuevo elemento en la cola
+			nodo *new_nd;
+			new_nd = new nodo;
+			new_nd->value = new_it;
+
+			// si la cola ya esta creada, el elemento siguiente
+			// al ultimo elemento actual ahora apunta al nuevo elemento
+			// y el ultimo elemento ahoraes ese nuevo elemento
+			if (tail_ != NULL) tail_->next = new_nd;
+			tail_ = new_nd;
+
+			// si la cola no esta creada, head_ apunta a NULL
+			// por tanto, al crearla apunta al nuevo elemento
+			if (head_ == NULL) head_ = new_nd;
+
+			assert(!this->is_empty());
+
+			// DONE
+		}
+
+		/** Remove the front item.
+		 * @pre not is_empty()
+		 * @post !is_empty() || size()==0
+		 */
+		void deque() {
+			//TODO
+			assert(!this->is_empty());
+
+			// para eliminar el primer elemento de la lista
+			// necesitamos que el puntero apunte a su siguiente elemento
+			// el primer elemento lo guardamos en un puntero auxiliar 
+			// para despues liberar memoria con delete
+			nodo *aux = head_;
+			head_ = head_->next;
+			delete aux;
+
+			// DONE
+		}
+
+		/** @} */
+
+	private:
+
+		/** @brief Copy constructor.
+		 * @warning we don't want a copy constructor.
+		 */
+		Queue(const Queue<T>& other)
+		{}
+
+		/** @brief Assign operator.
+		 * @warning we don't want the assign operator.
+		 */
+		Queue<T>& operator=(const Queue<T>& other)
+		{
+			return *this;
+		}
+
+	protected:
+		//TODO
+		struct nodo {
+			T value;
+			nodo *next;
+		};
+
+		nodo *head_;
+		nodo *tail_;
+		// DONE
+};
+
+#endif
