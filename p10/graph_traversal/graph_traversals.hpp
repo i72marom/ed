4075de8feeca9cc78da1b0edfa07b661e649df67
manuel mariscal <i72marom@uco.es>
2020-05-10 17:41:07 +0200

    p10 - ok - home

--- p10/graph_traversal/graph_traversals.hpp
+++ p10/graph_traversal/graph_traversals.hpp
@@ -19,15 +19,36 @@
 template<class T, class Processor>
 bool depth_first_search(WGraph<T>& g, Processor& p)
 {
-    assert(g.has_current_node());
-    bool ret_val = true;
+	assert(g.has_current_node());
+	bool ret_val = true;
 
-    //TODO
-    //Do an ITERATIVE implementation.
+	//TODO
+	//Do an ITERATIVE implementation.
+	typedef typename GraphNode<T>::Ref NodeRef;
 
+	std::stack <NodeRef> frontier;
+	NodeRef u, v;
+	
+	frontier.push(g.current_node());
 
+	while (!frontier.empty()){
+		u = frontier.top();
+		frontier.pop();
 
-    return ret_val;
+		if (!u->is_visited()) {
+			u->set_visited(true);
+			ret_val = p.apply(u);
+			g.goto_node(u);
+
+			while (g.has_current_edge()) {
+				v = g.current_edge()->other(u);
+				if (!v->is_visited()) frontier.push(v);
+				g.goto_next_edge();
+			}
+		}
+	}
+
+	return ret_val;
 }
 
 /**
@@ -43,12 +64,35 @@ bool depth_first_search(WGraph<T>& g, Processor& p)
 template<class T, class Processor>
 bool breadth_first_search(WGraph<T>& g, Processor& p)
 {
-    assert(g.has_current_node());
-    bool ret_val = true;
+	assert(g.has_current_node());
+	bool ret_val = true;
 
-    //TODO
+	//TODO
+	typedef typename GraphNode<T>::Ref NodeRef;
 
-    return ret_val;
+	std::queue <NodeRef> frontier;
+	NodeRef u, v;
+	
+	frontier.push(g.current_node());
+
+	while (!frontier.empty()){
+		u = frontier.front();
+		frontier.pop();
+
+		if (!u->is_visited()) {
+			u->set_visited(true);
+			ret_val = p.apply(u);
+			g.goto_node(u);
+
+			while (g.has_current_edge()) {
+				v = g.current_edge()->other(u);
+				if (!v->is_visited()) frontier.push(v);
+				g.goto_next_edge();
+			}
+		}
+	}
+
+	return ret_val;
 }
 
 #endif // __GRAPH_TRAVERSALS_HPP
