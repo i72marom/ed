50c28ecff7e5fef0343899412226bff8047bd988
manuel mariscal <i72marom@uco.es>
2020-05-10 16:41:41 +0200

    p10 - city - ok

--- p10/graph_traversal/city.cpp
+++ p10/graph_traversal/city.cpp
@@ -56,6 +56,46 @@ std::shared_ptr<WGraph<City>> create_wgraph(std::istream &in) noexcept(false)
     //TODO
     //Remenber: to save space, for the edges it is only used the name of de
     //city and not the whole city data.
+    // comprobamos si el grafo es dirigido o no dirigido
+    std::string is_directed;
+    in >> is_directed;
+    if (!in) std::runtime_error("Wrong graph");
+    
+    // obtenemos el numero de nodos
+    size_t n_nodes;
+    in >> n_nodes;
+    if (!in) std::runtime_error("Wrong graph");
+
+    // creamos el grafo
+    graph = std::shared_ptr<WGraph<City>>(new WGraph<City>(n_nodes));
+
+    // añadimos los nodos al grafo
+    for(size_t i=0; i<n_nodes; ++i) {
+        City item;
+        in >> item;
+        
+        if (!in) std::runtime_error("Wrong graph");
+        
+        graph->add_node(item);
+    }
+
+    // añadimos las conexiones en funcion de 
+    // si el grafo es dirigido o no dirigido
+    size_t n_edges;
+    in >> n_edges;
+    
+    for (size_t i=0;i<n_edges; ++i) {
+        std::string u, v;
+        float w;
+        
+        in >> u >> v >> w;
+        
+        graph->set_weight(graph->find(u), graph->find(v), w);
+        if (is_directed == "NON_DIRECTED") 
+            graph->set_weight(graph->find(v), graph->find(u), w);
+    }
+
+    return graph;
 
     return graph;
 }
