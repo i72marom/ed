4d52e5561ecefd1d8e9692da481af4d9d05ab720
manuel mariscal <i72marom@uco.es>
2020-05-12 17:29:41 +0200

    runtest - v1.1

--- p11/graph_minimum_spanning_tree/run_tests.sh
+++ p11/graph_minimum_spanning_tree/run_tests.sh
@@ -17,22 +17,17 @@ PASS=0
 TOTAL=0
 
 for t in ${TEST_DIR}/?? ; do
-	TMPFILE=/tmp/ed_test_$$_${RANDOM}
+        TMPFILE=/tmp/ed_test_$$_${RANDOM}
 	TOTAL=$(( $TOTAL + 1 ))
-	set +e
-	$prog $t >& ${TMPFILE}
-	RETV=$?
-	YOUR_OUTPUT="$( cat ${TMPFILE} )"
+        set +e
+        $prog $t >& ${TMPFILE}
+        YOUR_OUTPUT="$( cat ${TMPFILE} )"
 	EXPECTED_OUTPUT="$( cat ${t}.a )"
-	DIFF=$( diff $TMPFILE ${t}.a )
-	set -e
-        
-	echo -n "test $t : "
-	if [ $RETV != 0 ] || [ "${YOUR_OUTPUT}" != "${EXPECTED_OUTPUT}" ]; then
+        DIFF=$( diff $TMPFILE ${t}.a )
+        set -e
+        echo -n "test $t : "
+	if [ "${YOUR_OUTPUT}" != "${EXPECTED_OUTPUT}" ]; then
 		echo " Fail!"
-		if [ $RETV != 0 ]; then
-			echo -e "\t The program exits with error code $RETV"
-                fi
                 echo -e "\t Expected:\n[${EXPECTED_OUTPUT}]"
                 echo -e "\t Your output:\n[${YOUR_OUTPUT}]"
                 echo -e "\t Differences:\n[${DIFF}]"
@@ -43,6 +38,3 @@ for t in ${TEST_DIR}/?? ; do
 done
 
 echo "You pass $PASS of $TOTAL tests."
-
-exit 0
-
