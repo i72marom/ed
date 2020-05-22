17a18be6eae3da43d7e2bd6ea364298e700088f3
manuel mariscal <i72marom@uco.es>
2020-05-23 00:50:15 +0200

    p12 - ok

--- p12/maze_solver/a_star_algorithm.hpp
+++ p12/maze_solver/a_star_algorithm.hpp
@@ -26,60 +26,70 @@
 template<class T, class Distance>
 size_t
 a_star_algorithm(WGraph<T>& g,
-                 typename WGraph<T>::NodeRef const& start,
-                 typename WGraph<T>::NodeRef const& end,
-                 const Distance& distance,
-                 std::vector<size_t>& predecessors,
-                 std::vector<float>& distances)
+				 typename WGraph<T>::NodeRef const& start,
+				 typename WGraph<T>::NodeRef const& end,
+				 const Distance& distance,
+				 std::vector<size_t>& predecessors,
+				 std::vector<float>& distances)
 {
-    assert(g.has(start));
-    assert(g.has(end));
+	assert(g.has(start));
+	assert(g.has(end));
 
-    //Remember to use (dist-to-end,dist-front-start,u,v) format for the tuples due to they are lexicographicaly sorted.
-    //Use typedef to create an alias. So we'll have an easier code.
-    //TODO: typedef std::typle< ...... > Tuple;
+	//Remember to use (dist-to-end,dist-front-start,u,v) format for the tuples due to they are lexicographicaly sorted.
+	//Use typedef to create an alias. So we'll have an easier code.
+	//TODO: typedef std::typle< ...... > Tuple;
+	typedef std::tuple<float, float, typename WGraph<T>::NodeRef, typename WGraph<T>::NodeRef> Tuple;
 
-    //Initialize data.
-    size_t iterations = 0;
-    g.set_visited(false);
-    predecessors.resize(g.size());
-    for (size_t i=0;i<predecessors.size();++i)
-        predecessors[i]=i;
-    distances.resize(g.size(), std::numeric_limits<float>::infinity());
+	//Initialize data.
+	size_t iterations = 0;
+	g.set_visited(false);
+	predecessors.resize(g.size());
+	for (size_t i=0;i<predecessors.size();++i)
+		predecessors[i]=i;
+	distances.resize(g.size(), std::numeric_limits<float>::infinity());
 
-    //TODO: Create a priority queue. Remember we want to pick up the lowest item into the queue.
+	//TODO: Create a priority queue. Remember we want to pick up the lowest item into the queue.
+	std::priority_queue <Tuple> p_queue;
 
-    //TODO: Insert the first item. You can use std::make_tuple(...) for this.
+	//TODO: Insert the first item. You can use std::make_tuple(...) for this.
+	p_queue.push(std::make_tuple(distance(start, end), 0.0, start, start));
 
+	//While there are nodes to visit and the end node has not been achieved ...
+	while (!(p_queue.empty() || end->is_visited())) {
+		iterations ++;
 
-    //While there are nodes to visit and the end node has not been achieved ...
-    while (false /* replace by a proper check*/ )
-    {
-        iterations ++;
+		// TODO: Get the next tuple to process and
+		// remove the tuple from the queue.
+		Tuple t = p_queue.top();		
+		p_queue.pop();
 
-        //TODO: Get the next tuple to process.
+		//TODO: Set u as the next vertex to visit.
+		auto u = std::get<2>(t);
+		g.goto_node(u);
 
-        //TODO: Remove the tuple from the queue.
+		//If u is not visited yet, it will be processed.
+		if (!u->is_visited()) {
+			// Set u as visited.
+			u->set_visited(true);
+			
+			// Update predessors[u] and distances[u]
+			predecessors[u->label()] = std::get<3>(t)->label();
+			distances[u->label()] = std::get<1>(t);
 
-        //TODO: Set u as the next vertex to visit.
+			//Add new tuples for not visited vertices linked with u.
+			while (g.has_current_edge()) {
+				auto v = g.current_edge()->other(u);
+				if (!v->is_visited())
+					p_queue.push(std::make_tuple(
+						distances[u->label()] + g.current_edge()->item() + distance(v, end),
+						distances[u->label()] + g.current_edge()->item(), v, u));
 
+				g.goto_next_edge();
+			}
+		}
+	}
 
-        //If u is not visited yet, it will be processed.
-        if (false /*replace by a proper check*/)
-        {
-            //Set u as visited.
-            //Update predessors[u]
-            //Update distances[u]
-            //Add new tuples for not visited vertices linked with u.
-            //Le v one of these vertices. Remenber that:
-            //0 -> estimated_dist = distance_from_start_V + heuristic(v, end).
-            //1 -> distance_from_start_V = dist_from_start_U + edge(u,v)->item
-            //2 -> Next vertex is v.
-            //3 -> Predecessor is u.
-        }
-    }
-
-    return iterations;
+	return iterations;
 }
 
 #endif // A_STAR_ALGORITHM_HPP
--- p12/maze_solver/maze_solver.cpp
+++ p12/maze_solver/maze_solver.cpp
@@ -2,6 +2,7 @@
 #include "maze_solver.hpp"
 
 #include <stack>
+#include <list>
 #include <cmath>
 
 /**
@@ -11,52 +12,67 @@
  * @return a tuple <x,y>.
  */
 static inline std::tuple<int, int>
-label_to_grid_coord(size_t v, int grid_width)
-{
-    //TODO
-    return std::make_tuple(0, 0);
+label_to_grid_coord(size_t v, int grid_width) {
+	//TODO
+	return std::make_tuple(v % grid_width, v / grid_width);
+	// DONE
 }
 
 /**
  * @brief Functional to compute the euclidean distance between graph vertices.
  */
-struct Distance
-{
-    Distance(/* arguments? */)
-    {
-        //TODO
-    }
+struct Distance {
+	size_t size_;
 
-    float operator()(const WGraph<int>::NodeRef& a,
-                     const WGraph<int>::NodeRef& b) const
-    {
-        //TODO
-        return 0.0f;
-    }
+	Distance(size_t size) {
+		//TODO
+		size_t size_ = size;
+		// DONE
+	}
+
+	float operator()(const WGraph<int>::NodeRef& a, const WGraph<int>::NodeRef& b) const {
+		//TODO
+		std::tuple <int, int> coord_a = label_to_grid_coord(a->label(), size_);
+		std::tuple <int, int> coord_b = label_to_grid_coord(b->label(), size_);
+
+		return sqrt(pow(std::get<0>(coord_b) - std::get<0>(coord_a), 2) + pow(std::get<1>(coord_b) - std::get<1>(coord_a), 2));
+		// DONE
+	}
 };
 
 void
-maze_solver(WGraph<int>& maze,
-            WGraph<int>::NodeRef& start,
-            WGraph<int>::NodeRef& end,
-            const int grid_width,
-            std::vector<std::tuple<int, int> > &path)
-{
-    std::vector<size_t> predecesors;
-    std::vector<float> distances;
+maze_solver(WGraph<int>& maze, 
+			WGraph<int>::NodeRef& start, 
+			WGraph<int>::NodeRef& end, 
+			const int grid_width, 
+			std::vector<std::tuple<int, int> > &path) {
+	
+	std::vector<size_t> predecesors;
+	std::vector<float> distances;
 
-    //TODO
-    //Call to a*  with the proper arguments.
+	//TODO
+	//Call to a*  with the proper arguments.
+	a_star_algorithm(maze, start, end, Distance(grid_width), predecesors, distances);
 
+	//
+	path.resize(0);
 
-    //
-    path.resize(0);
+	if (end->is_visited()) {
+		//If there is a path, save it into path vector.
+		//Remenber: the result will be tuples <x,y> for 
+		//the all the points in the path from start point to end point.
 
-    if (false /*replace with a comparison to see if a route was found.*/)
-    {
-        //If there is a path, save it into path vector.
-        //Remenber: the result will be tuples <x,y> for the all the points in the path from start point to end point.
+		//TODO
+		size_t p = predecesors.back();
+		std::list <std::tuple<int, int>> aux;
+		aux.push_front(label_to_grid_coord(end->label(), grid_width));
+		
+		while (p != start->label()) {
+			aux.push_front(label_to_grid_coord(p, grid_width));
+			p = predecesors[p];
+		}
 
-        //TODO
-    }
+		aux.push_front(label_to_grid_coord(start->label(), grid_width));
+		path.assign(aux.begin(), aux.end());
+	}
 }
